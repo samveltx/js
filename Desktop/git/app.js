f54e2807682dd8023dd95d08b297ccd2c5c01a38
SoniaHakobian <sonyahakobyan97@gmail.com>
2017-10-13 16:50:11 +0400

    Added comment

---
+++ Desktop/git/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+      res.send('The GET request is coming');
+        console.log("Request is coming");
+})
+
+app.post('/', function (req, res) {
+      res.send('The POST request is coming');
+        console.log("Request is coming");
+})
+//i am Sonia
+app.put('/', function (req, res) {
+      res.send('The PUT request is coming');
+        console.log("Request is coming");
+})
+
+app.delete('/', function (req, res) {
+      res.send('The DELETE request is coming');
+        console.log("Request is coming");
+})
+app.listen(3333);
